a43cf87be8271ca7ed83e60e1a05d2d59f3f1ecf
Kenyon <kengeet56@gmail.com>
2023-09-20 11:40:21 -0500

    Create RedBlackTree.py

---
+++ RedBlackTree.py
@@ -0,0 +1,94 @@
+class Node:
+    def __init__(self, key, color, left=None, right=None, parent=None):
+        self.key = key
+        self.color = color
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+class RedBlackTree:
+    def __init__(self):
+        self.NIL_LEAF = Node(None, 'black')
+        self.root = self.NIL_LEAF
+
+def insert(self, key):
+    new_node = Node(key, 'red', self.NIL_LEAF, self.NIL_LEAF)
+    if self.root == self.NIL_LEAF:
+        self.root = new_node
+        self.root.color = 'black'
+    else:
+        self._insert_recursive(self.root, new_node)
+
+def _insert_recursive(self, current, new_node):
+    if new_node.key < current.key:
+        if current.left == self.NIL_LEAF:
+            current.left = new_node
+            new_node.parent = current
+            self._insert_fixup(new_node)
+        else:
+            self._insert_recursive(current.left, new_node)
+    else:
+        if current.right == self.NIL_LEAF:
+            current.right = new_node
+            new_node.parent = current
+            self._insert_fixup(new_node)
+        else:
+            self._insert_recursive(current.right, new_node)
+
+def _insert_fixup(self, node):
+    while node.parent.color == 'red':
+        if node.parent == node.parent.parent.left:
+            uncle = node.parent.parent.right
+            if uncle.color == 'red':
+                node.parent.color = 'black'
+                uncle.color = 'black'
+                node.parent.parent.color = 'red'
+                node = node.parent.parent
+            else:
+                if node == node.parent.right:
+                    node = node.parent
+                    self._left_rotate(node)
+                node.parent.color = 'black'
+                node.parent.parent.color = 'red'
+                self._right_rotate(node.parent.parent)
+        else:
+            uncle = node.parent.parent.left
+            if uncle.color == 'red':
+                node.parent.color = 'black'
+                uncle.color = 'black'
+                node.parent.parent.color = 'red'
+                node = node.parent.parent
+            else:
+                if node == node.parent.left:
+                    node = node.parent
+                    self._right_rotate(node)
+                node.parent.color = 'black'
+                node.parent.parent.color = 'red'
+                self._left_rotate(node.parent.parent)
+    self.root.color = 'black'
+
+def inorder_traversal(self, node):
+    if node != self.NIL_LEAF:
+        self.inorder_traversal(node.left)
+        print(f"Key: {node.key}, Color: {node.color}")
+        self.inorder_traversal(node.right)
+
+def main():
+    tree1 = RedBlackTree()
+    keys1 = [41, 38, 31, 12, 19, 8]
+    for key in keys1:
+        tree1.insert(key)
+
+    print("In-Order Traversal of Tree 1:")
+    tree1.inorder_traversal(tree1.root)
+
+    tree2 = RedBlackTree()
+    keys2 = [11, 2, 14, 1, 7, 15, 5, 8, 4]
+    for key in keys2:
+        tree2.insert(key)
+
+    print("In-Order Traversal of Tree 2:")
+    tree2.inorder_traversal(tree2.root)
+
+if __name__ == "__main__":
+    main()
